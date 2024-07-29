0a1038455b9418882d436024c87690e1dd159765
Mayank Thakur <mayank.thakur@therealbrokerage.com>
2024-07-29 15:35:25 +0530

    updated vervcel

--- app.js
+++ app.js
@@ -14,16 +14,14 @@ mongoDb
     console.log("error connecting");
   });
 const app = mimi();
-// Serve static files (e.g., HTML, CSS, JS)
-app.use(express.static(path.join(__dirname, "public")));
 
 // Create a route to serve HTML files
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(3000, () => {
-  console.log("sever running port 3000");
+app.listen(process.env.PORT, () => {
+  console.log("sever running port 3200");
 });
 
 module.exports = app;
